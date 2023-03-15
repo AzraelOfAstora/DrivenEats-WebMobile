72c995c1c74bf25ef22bf772ba8160dcdf40c6f5
Vinicius <viniciusmoura761@gmail.com>
2023-03-15 16:54:29 -0300

    Finalizar pedido e enviar mensagem via whatsapp

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const pedidos = document.querySelectorAll('.boxPedido');
+const botao = document.getElementById('fecharPedido');
 pedidos.forEach(Element => Element.addEventListener('click', selecionarPedido));
 function selecionarPedido(e) {
     if (this.classList.contains("selecionado")) {
@@ -39,13 +40,27 @@ function selecionarPedido(e) {
         check.setAttribute('id', 'check');
         this.classList.add('selecionado');
         this.children[3].appendChild(check);
-        const pedidosSelecionados = document.querySelectorAll('.selecionado')
-        if (pedidosSelecionados.length == 3){
-            const botao = document.getElementById('fecharPedido');
-            botao.textContent = "Fechar pedido";
-            botao.removeAttribute('id', 'fecharPedido');
-            botao.setAttribute('class', 'fecharPedido');
-        }
     }
 }
-
+window.addEventListener('click', ativarBotao);
+function ativarBotao() {
+    const pedidosSelecionados = document.querySelectorAll('.selecionado');
+    if (pedidosSelecionados.length == 3) {
+        botao.removeAttribute('id', 'fecharPedido');
+        botao.setAttribute('class', 'fecharPedido');
+        botao.textContent = "Fechar pedido";
+        let pedidoFinal = [];
+        for(let i = 0; i< pedidosSelecionados.length; i++){
+            pedidoFinal.push(pedidosSelecionados[i].children[1].textContent);
+        }
+        let precoFinal = 0;
+        for (let i = 0; i<pedidosSelecionados.length;i++){
+           precoFinal += parseFloat(pedidosSelecionados[i].children[3].children[0].lastChild.innerText.replace(',', '.'));
+        }
+        let precoFinalFixed = precoFinal.toFixed(2);
+        let mensagemDoPedido = encodeURIComponent(`Olá, gostaria de fazer o pedido:\n- Prato: ${pedidoFinal[0]}\n- Bebida: ${pedidoFinal[1]}\n- Sobremesa: ${pedidoFinal[2]}\nTotal: R$ ${precoFinalFixed}`);
+        botao.addEventListener('click', function (){
+            window.location.assign(`https://wa.me/5511960386578?text=${mensagemDoPedido}`);
+        })
+    }
+}
